9bff1dfdb854630ed877cab22cf66497810f79d7
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2025-01-14 12:23:27 +0100

    TDT4100: Add assignment 0

---
+++ TDT4100/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author hal
+ *
+ */
+open module assignments {
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+}
---
+++ TDT4100/src/test/java/assignment0/HelloWorldTest.java
@@ -0,0 +1,28 @@
+package assignment0;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import assignment0.HelloWorld;
+
+public class HelloWorldTest {
+
+    private HelloWorld helloWorld;
+
+    @BeforeEach
+    public void setUp() {
+        this.helloWorld = new HelloWorld();
+    }
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello World!", this.helloWorld.getHelloWorld());
+    }
+
+    @Test
+    public void testHelloWorldLength() {
+        assertEquals(12, this.helloWorld.getHelloWorldLength());
+    }
+}
